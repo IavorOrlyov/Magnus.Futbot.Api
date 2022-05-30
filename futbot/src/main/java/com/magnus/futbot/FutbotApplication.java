a81b02bb262f6b4a5378e41db981c2ccdaf4a455
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-05-30 08:34:12 +0300

    Initial create

---
+++ futbot/src/main/java/com/magnus/futbot/FutbotApplication.java
@@ -0,0 +1,13 @@
+package com.magnus.futbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbotApplication.class, args);
+	}
+
+}
---
+++ futbot/src/test/java/com/magnus/futbot/FutbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnus.futbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
