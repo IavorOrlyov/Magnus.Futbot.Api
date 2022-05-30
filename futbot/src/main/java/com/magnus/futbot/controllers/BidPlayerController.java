6e7108fb6c551b2057d541d3cd1022c4b92f48e3
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-05-30 08:34:06 +0300

    Initial create

---
+++ futbot/src/main/java/com/magnus/futbot/controllers/BidPlayerController.java
@@ -0,0 +1,13 @@
+package com.magnus.futbot.controllers;
+
+import javax.servlet.annotation.HttpConstraint;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BidPlayerController {
+    @RequestMapping("/")
+    @HttpConstraint()
+    public 
+}
