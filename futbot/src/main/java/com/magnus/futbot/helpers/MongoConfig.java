fc8514b393600323210d65283423a5d901540fa4
Iavor Orlyov <i.orlyov@drafkings.com>
2022-06-08 14:53:32 +0300

    Db configuration set in environment variables

--- futbot/src/main/java/com/magnus/futbot/helpers/MongoConfig.java
+++ futbot/src/main/java/com/magnus/futbot/helpers/MongoConfig.java
@@ -4,21 +4,37 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.Map;
+
 @Configuration
 @EnableMongoRepositories(basePackages = "org.spring.mongo.demo")
 public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${DbUsername}")
+    private String username;
+    @Value("${DbPassword}")
+    private String password;
+    @Value("${DbCluster}")
+    private String cluster;
+    @Value("${DatabaseName}")
+    private String databaseName;
+
     @Override
     protected String getDatabaseName() {
-        return "futbot-java";
+        return databaseName;
     }
 
     @Override
     public MongoClient mongoClient() {
-        final ConnectionString connectionString = new ConnectionString("mongodb+srv://FidoDidoo100:<password>@fbcluster.rdkdn.mongodb.net/futbot-java");
+
+        final ConnectionString connectionString =
+                new ConnectionString(String.format("mongodb+srv://%s:%s@%s.rdkdn.mongodb.net/%s",
+                        username, password, cluster, databaseName));
         final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
